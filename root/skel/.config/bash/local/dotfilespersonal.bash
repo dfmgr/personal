fb26b5dcf7b4a8a44c9822f2cb9a066f34f2e1b7
Jason <jason@casjay.net>
2021-11-17 10:36:30 -0500

    🐛 FIXED: Installer 🐛

--- root/skel/.config/bash/local/dotfilespersonal.bash
+++ root/skel/.config/bash/local/dotfilespersonal.bash
@@ -1,5 +1,11 @@
+#!/usr/bin/env bash
+
 dotfilespersonal() {
-local MIN=no 
-local UPDATE=yes
-bash -c "$(curl -LSs -H 'Authorization: token $GITHUB_ACCESS_TOKEN' $MYPERSONALGITREPO/raw/main/install.sh)"
+  local MIN="${PERSONAL_UPDATE_MIN:-no}"
+  local UPDATE="${PERSONAL_UPDATE:-yes}"
+  local TOKEN="${GITHUB_ACCESS_TOKEN:-MYPERSONAL_GIT_TOKEN}"
+  local REPO="${MYPERSONAL_GIT_REPO:-}"
+  [ -z "${TOKEN}" ] && { printf '\t\tYour auth TOKEN is not set - env:MYPERSONAL_GIT_TOKEN' && return 1; }
+  [ -z "${REPO}" ] && { printf '\t\tYour personal repo is not set - env:MYPERSONAL_GIT_REPO' && return 1; }
+  bash -c "$(curl -LSs -H 'Authorization: token '$TOKEN'' $MYPERSONAL_GIT_REPO/raw/main/install.sh)"
 }
--- root/skel/README.md
+++ root/skel/README.md
@@ -1,4 +1,4 @@
 ## SKEL Dir  
+
 Copy any of your personal from  
 your home directory to this folder  
-
